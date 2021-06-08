37470eda61b74dfc613162c78fed26fbaf6ac2cb
MauriPiace92 <75382063+MauriPiace92@users.noreply.github.com>
2021-06-08 18:03:53 +0200

    fine header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script defer src="https://pro.fontawesome.com/releases/v5.10.0/js/all.js" integrity="sha384-G/ZR3ntz68JZrH4pfPJyRbjW+c0+ojii5f+GYiYwldYU69A+Ejat6yIfLSxljXxD" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
     <title>Document</title>
 
     <link rel="stylesheet" href="{{ asset('css/app.css')}}">
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,57 @@
 <header>
-    sono HEader
+    
+    {{-- line blue over header --}}
+    <div class="upper-header">
+        <div class="container">
+            <a href="#">DC POWER </a>
+            <a href="#">ADDITIONAL DC SITES <i class="fas fa-sort-down"></i></a>
+        </div>
+    </div>
+    {{-- HEADER --}}
+    <div class="container">
+        <div class="header-menu">
+            <div class="logo">
+                <img src="{{ asset('../img/dc-logo.png')}}" alt="logo">
+            </div>
+
+            <nav>
+                <ul class="menu">
+                    <li>
+                        <a href="#">charachters</a>
+                    </li>
+                    <li class="active">
+                        <a class="active" href="#">comics</a>
+                    </li>
+                    <li>
+                        <a href="#">movies</a>
+                    </li>
+                    <li>
+                        <a href="#">tv</a>
+                    </li>
+                    <li>
+                        <a href="#">games</a>
+                    </li>
+                    <li>
+                        <a href="#">collectibles</a>
+                    </li>
+                    <li>
+                        <a href="#">videos</a>
+                    </li>
+                    <li>
+                        <a href="#">fans</a>
+                    </li>
+                    <li>
+                        <a href="#">shop <i class="freccia fas fa-sort-down"></i></a>
+                    </li>
+                
+                    <li class="search-bar">
+                        <input type="text" placeholder="search">
+                        <i class="fas fa-search"></i>
+                    </li>
+                </ul>
+            </nav>
+            
+        </div>    
+    </div>
+    
 </header>
